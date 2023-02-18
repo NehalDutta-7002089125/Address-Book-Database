2f3103946147aa57558a469366ca7d319e7ca3ac
“NehalDutta-7002089125” <nihal326dutta@gmail.com>
2023-02-18 21:39:36 +0530

    [Nehal]Add- Create a table

--- AddressBookDatabase.sql
+++ AddressBookDatabase.sql
@@ -1,1 +1,4 @@
 ﻿Create database Address_Book_System
+create Table Address_Book
+(FirstName varchar(100), LastName varchar(100), Address varchar(300), 
+City varchar(50), State varchar(30), ZipCode int, PhoneNumber varchar, EMailId varchar(50) )
