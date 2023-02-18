96c841e86966e74045d1b8a4d12b77d00b42f3a3
“NehalDutta-7002089125” <nihal326dutta@gmail.com>
2023-02-18 21:49:39 +0530

    [Nehal]Add- Create values into the table

--- AddressBookDatabase.sql
+++ AddressBookDatabase.sql
@@ -2,3 +2,11 @@
 create Table Address_Book
 (FirstName varchar(100), LastName varchar(100), Address varchar(300), 
 City varchar(50), State varchar(30), ZipCode int, PhoneNumber varchar, EMailId varchar(50) )
+
+insert into Address_Book values
+('Nehal', 'Dutta', 'Jorhat', 'Guwahati', 'Assam', 76500, 98755555, 'nihal@gmail.com'),
+('raj', 'dav', 'Kullu', 'Manali', 'Himachal', 47984, 897621110, 'raj221@gmail.com'),
+('Kajal', 'Mane', 'Degav', 'Karad', 'Gujrat', 123321, 9876543210, 'kaj12@gmail.com'),
+('Vikram', 'Pawar', 'Vaduj', 'Pune', 'Rahsthan', 222333,9011973823, 'viki12@gmail.com'),
+('Ranjit', 'More', 'Phaltan', 'Mumbai', 'Goa', 333444, 4545676732, 'ranjit43@gmail.com')
+
